5e6bcc47222b480ad91a07528e5c431fb09ff058
Danniel Navas <dannielnavas@gmail.com>
2024-12-06 07:21:20 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Link, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+export default App;
